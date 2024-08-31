b6f3d015e399b57d2e74a01a55e2510652dfb9e0
Cavidan-09 <cavidanmesimli6@gmail.com>
2024-08-31 18:42:12 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let user =prompt("Tesvir daxil edin")
+switch(user){
+    case "12 may 2019-cu il":
+    console.log("Tesvir duzdu")
+    break;
+  case "25-dekabr 1456 -ci il":
+  console.log("Tesvir Duzdu")
+  break;
+  case "2024.08.27":
+  console.log("Tesvir Duzdu")
+  break;
+  default:
+    console.log("Tesvir sefdi");
+}
+
